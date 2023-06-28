64ce81cdf9524e747613d9b529ec22ec1816100c
farruxchan <farrux.milan@gmail.com>
2023-06-28 16:24:54 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let students = ['Umar', 'Danil','Zohid', 'Farrukh', 'Abdumalik', 'Temur', 'Temuraka', 'Vsevolod']
+alert (students)
+
+students.push ('Aziz', 'Zuhriddin');
+console.log (students);
+alert (students)
